730055041bd5dc533ab0ada836b57a2cdde36ad4
Danillo Ilggner <danilloilggner@gmail.com>
2023-03-10 14:24:42 -0300

    inject jwt service and user repo on main instead of root middleware

--- app/server/main.dart
+++ app/server/main.dart
@@ -1,9 +1,12 @@
 import 'dart:io';
 
 import 'package:dart_frog/dart_frog.dart';
+import 'package:data_source/data_source.dart';
 import 'package:database/database.dart';
 import 'package:dotenv/dotenv.dart';
+import 'package:repository/repository.dart';
 import 'package:server/core/environment.dart';
+import 'package:server/services/jwt_service.dart';
 
 final _env = Environment(DotEnv());
 final _dbOptions = DatabaseOptions(
@@ -14,14 +17,31 @@ final _dbOptions = DatabaseOptions(
   username: _env.dbUsername,
 );
 
+PgConnection get _db => PgClient(_dbOptions);
+
 Future<HttpServer> run(Handler handler, InternetAddress ip, int port) {
   return serve(
-    handler.use(provider<Environment>((context) => _env)).use(
-          provider<PgConnection>(
-            (context) => PgClient(_dbOptions),
+    handler
+        .use(provider<Environment>((_) => _env))
+        .use(provider<PgConnection>((_) => _db))
+        .use(provider<JwtService>((context) => _jwt))
+        .use(
+          provider<UserRepository>(
+            (_) => _userRepo(
+              UserDataSourceImpl(_db),
+              const PasswordHasherService(),
+            ),
           ),
         ),
     ip,
     port,
   );
 }
+
+JwtService get _jwt => JwtService(_env);
+
+UserRepository _userRepo(
+  UserDataSource dataSrc,
+  PasswordHasherService psswrdHasher,
+) =>
+    UserRepositoryImpl(dataSrc, psswrdHasher);
--- app/server/routes/users/_middleware.dart
+++ app/server/routes/users/_middleware.dart
@@ -1,17 +1,11 @@
 import 'package:dart_frog/dart_frog.dart';
-import 'package:data_source/data_source.dart';
-import 'package:repository/repository.dart';
 import 'package:server/controllers/user_controller.dart';
-import 'package:server/services/jwt_service.dart';
 
 Handler middleware(Handler handler) => handler.use(
       provider<UserController>(
         (context) => UserController(
-          UserRepositoryImpl(
-            UserDataSourceImpl(context.read()),
-            const PasswordHasherService(),
-          ),
-          JwtService(context.read()),
+          context.read(),
+          context.read(),
         ),
       ),
     );
